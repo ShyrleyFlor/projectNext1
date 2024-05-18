a513fce28e756e75bcab781abb0c69aa0e1986ee
Shirley Natalia Benitez <shirleybenites@romapy.local>
2024-05-18 13:36:07 -0400

    suspense

--- projectnext1/app/posts/[id]/page.jsx
+++ projectnext1/app/posts/[id]/page.jsx
@@ -1,21 +1,27 @@
-
+import Posts from "../page";
+import { Suspense } from "react";
+import Loading from "@/app/loading";
 async function loadPost(id) {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-    const post = await res.json()
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const post = await res.json();
 
-    return post
+  return post;
 }
 async function PostID({ params }) {
+  const post = await loadPost(params.id);
 
-    const post = await loadPost(params.id)
-
-    return (
-        <div>
-            <h2>{post.id}</h2>
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
-        </div>
-    );
+  return (
+    <div>
+      <h2>{post.id}</h2>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <hr />
+      <h3>Otras publicaciones</h3>
+      <Suspense fallback={<Loading />}>
+        <Posts />
+      </Suspense>
+    </div>
+  );
 }
 
-export default PostID
+export default PostID;
--- projectnext1/app/posts/page.jsx
+++ projectnext1/app/posts/page.jsx
@@ -1,4 +1,5 @@
 import PostCard from "@/components/PostCard";
+
 async function loadPosts() {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
     const posts = await res.json();
